6100c738f624083b3fb7c1cee9dfde184250fac8
Mahesh Khedkar <maheshmk0807@gmail.com>
2023-01-28 17:47:05 +0530

    Answer 2

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -38,10 +38,6 @@ public class WhatsappRepository {
         this.customGroupCount = customGroupCount;
     }
     public String createUser(String name, String mobile) throws Exception {
-        /*
-        If the mobile number exists in database, throw "User already exists" exception
-        Otherwise, create the user and return "SUCCESS"
-        */
         if(userHashMap==null || userHashMap.containsKey(mobile)) {
             //THROW EXCEPTION
             throw new Exception("User already exists");
@@ -49,23 +45,13 @@ public class WhatsappRepository {
         else{
             //CREATE USER
             userHashMap.put(name,new User(name,mobile));
-            return "SUCCESS";
+            return "success";//c
         }
     }
 
     public Group createGroup(List<User> users) {
-        /*
-        The list contains at least 2 users where the first user is the admin. A group has exactly one admin.
-         If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
-         If there are 2+ users, the name of group should be "Group count". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
-         Note that a personal chat is not considered a group and the count is not updated for personal chats.
-         If group is successfully created, return group.
-
-        For example: Consider userList1 = {Alex, Bob, Charlie}, userList2 = {Dan, Evan}, userList3 = {Felix, Graham, Hugh}.
-        If createGroup is called for these userLists in the same order, their group names would be "Group 1", "Evan", and "Group 2" respectively.
-        */
         if(users.size()>2){
-            Group group = new Group("Group "+customGroupCount,users.size());//may throw exception if size is 1
+            Group group = new Group("Group "+customGroupCount+1,users.size());//may throw exception if size is 1
             groupUserMap.put(group,users);
             customGroupCount++;
             adminMap.put(group,users.get(0));
@@ -81,33 +67,23 @@ public class WhatsappRepository {
     }
 
     public int createMessage(String content) {
-        /*
-         The 'i^th' created message has message id 'i'.
-         Return the message id.
-         */
-        //Message message = new Message(messageId,content);
         messageId++;
         return messageId;
     }
 
     public int sendMessage(Message message, User sender, Group group) throws Exception {
-        /*
-        Throw "Group does not exist" if the mentioned group does not exist
-        Throw "You are not allowed to send message" if the sender is not a member of the group
-        If the message is sent successfully, return the final number of messages in that group.
-         */
         if(groupUserMap.containsKey(group)){
             if(groupUserMap.get(group).contains(sender)){
                 //send message
                 if(groupMessageMap.containsKey(group))//No prev msg in group
                 {
                     groupMessageMap.get(group).add(message);
-                    return groupMessageMap.get(group).size();
+                    return groupMessageMap.get(group).size()+1;
                 }
                 else{ //fetch prev
                     List<Message> msgs = new ArrayList<>();
                     msgs.add(message);
-                    return msgs.size();
+                    return msgs.size()+1;
                 }
             }
             else
@@ -118,12 +94,6 @@ public class WhatsappRepository {
     }
 
     public String changeAdmin(User approver, User user, Group group) throws Exception {
-        /*
-        Throw "Group does not exist" if the mentioned group does not exist
-        Throw "Approver does not have rights" if the approver is not the current admin of the group
-        Throw "User is not a participant" if the user is not a part of the group
-        Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
-         */
         if(adminMap.containsKey(group)&&groupUserMap.containsKey(group)){
             if(adminMap.get(group).equals(approver)){
                 if(groupUserMap.get(group).contains(user)){
@@ -131,12 +101,12 @@ public class WhatsappRepository {
                     return "success";
                 }
                 else //not group member
-                    throw new Exception("Not group member");
+                    throw new Exception("User is not a participant");//c
             }
             else ///No Admin
-                throw new Exception("You are not admin");
+                throw new Exception("Approver does not have rights");//
         }
         else //no group found
-            throw new Exception("Group not found");
+            throw new Exception("Group does not exist");//c
     }
 }
